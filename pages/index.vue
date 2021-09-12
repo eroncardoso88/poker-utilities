50c0a04e25f3074534fd6304f0a8337262076c7f
eroncardoso <deandradeeron@gmail.com>
2021-09-12 08:30:27 -0300

    homepage created

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <h4 v-if="mariana === true">Mariana will study VueJs with Nuxt</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      mariana: false,
+    };
+  },
+  computed: {},
+  methods: {
+    toggleMariana() {
+      setTimeout(() => {
+        this.mariana = !this.mariana;
+      }, 2000);
+    },
+  },
+  created: function () {
+    this.toggleMariana();
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
